2f01f1074d30d4a6bcda1e972efb37d244d29299
Thomas Hansen <dryvnt@gmail.com>
2016-07-03 14:15:33 +0200

    Restructured database 'get' function.
    Needed for double-faced card

--- src/card/database.rs
+++ src/card/database.rs
@@ -37,8 +37,9 @@ fn make_database() -> Database {
 
     let good_map: BTreeMap<String, DatabaseEntry> = BTreeMap::from_iter(bad_map.iter()
         .map(|(key, value)| (key.clone(), value.clone()))
-        .filter(&|(_, ref value)| {
-            vec!["normal", "split", "flip", "double-faced", "leveler"].contains(value.layout)
+        .filter(|&(_, ref value)| {
+            vec!["normal", "split", "flip", "double-faced", "leveler"]
+                .contains(&value.layout.as_str())
         })
         .map(|(key, value)| (key.replace("\u{fb}", "u"), value))); // û -> u, example: Lim-Dûl the Necromancer
 
@@ -49,53 +50,57 @@ lazy_static!{
     pub static ref DATABASE: Database = make_database();
 }
 
-impl Database {
-    pub fn get(&self, card_name: &str) -> Result<Card, ProxygenError> {
-        let entry = match self.map.get(card_name) {
-            Some(v) => v.clone(),
-            None => return Err(ProxygenError::InvalidCardName(String::from(card_name))),
-        };
-
-
-        // TODO: Get rid of these clones, jesus christ
-
-        match entry.layout.as_str() {
-            "normal" => {
-                let types = entry.types.unwrap_or_default();
-                if types.contains(&String::from("Creature")) {
-                    Ok(Card::Creature {
-                        name: entry.name,
-                        manacost: entry.manaCost.unwrap_or_default(),
-                        typeline: entry.sanetype.unwrap_or_default(),
-                        text: entry.text.unwrap_or_default(),
-                        power: entry.power.unwrap_or_default(),
-                        toughness: entry.toughness.unwrap_or_default(),
-                    })
-                } else if types.contains(&String::from("Planeswalker")) {
-                    Ok(Card::Planeswalker {
-                        name: entry.name,
-                        manacost: entry.manaCost.unwrap_or_default(),
-                        typeline: entry.sanetype.unwrap_or_default(),
-                        text: entry.text.unwrap_or_default(),
-                        loyalty: entry.loyalty.unwrap_or_default(),
-                    })
-                } else {
-                    Ok(Card::Noncreature {
-                        name: entry.name,
-                        manacost: entry.manaCost.unwrap_or_default(),
-                        typeline: entry.sanetype.unwrap_or_default(),
-                        text: entry.text.unwrap_or_default(),
-                    })
-
-                }
-            }
-            _ => {
-                Ok((Card::Unimplemented {
+fn parse_card(entry: DatabaseEntry) -> Card {
+    match entry.layout.as_str() {
+        "normal" => {
+            let types = entry.types.unwrap_or_default();
+            if types.contains(&String::from("Creature")) {
+                Card::Creature {
                     name: entry.name,
-                    layout: entry.layout,
-                }))
+                    manacost: entry.manaCost.unwrap_or_default(),
+                    typeline: entry.sanetype,
+                    text: entry.text.unwrap_or_default(),
+                    power: entry.power.unwrap_or_default(),
+                    toughness: entry.toughness.unwrap_or_default(),
+                }
+            } else if types.contains(&String::from("Planeswalker")) {
+                Card::Planeswalker {
+                    name: entry.name,
+                    manacost: entry.manaCost.unwrap_or_default(),
+                    typeline: entry.sanetype,
+                    text: entry.text.unwrap_or_default(),
+                    loyalty: entry.loyalty.unwrap_or_default(),
+                }
+            } else {
+                Card::Noncreature {
+                    name: entry.name,
+                    manacost: entry.manaCost.unwrap_or_default(),
+                    typeline: entry.sanetype,
+                    text: entry.text.unwrap_or_default(),
+                }
+
             }
         }
+        _ => {
+            Card::Unimplemented {
+                name: entry.name,
+                layout: entry.layout,
+            }
+        }
+    }
+}
 
+impl Database {
+    fn get_entry(&self, card_name: &str) -> Result<DatabaseEntry, ProxygenError> {
+        match self.map.get(card_name) {
+            Some(v) => Ok(v.clone()),
+            None => Err(ProxygenError::InvalidCardName(String::from(card_name))),
+        }
+    }
+
+    pub fn get(&self, card_name: &str) -> Result<Card, ProxygenError> {
+        let entry = try!(self.get_entry(card_name));
+
+        Ok(parse_card(entry))
     }
 }
